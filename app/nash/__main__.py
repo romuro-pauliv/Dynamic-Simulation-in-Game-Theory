0f5e44809d13dacf10031fcb4e0f6cf9aa3cf274
Romulo Pauliv <romulopauliv@bk.ru>
2024-02-23 21:38:34 -0300

    added save and load .bin

--- app/nash/__main__.py
+++ app/nash/__main__.py
@@ -1,42 +1,29 @@
 from calc.payoff_generation import PayoffGen
 from controller.choices import Choices
-from icecream import ic
+from resources.generate_bin import GenBin
 import numpy as np
+from icecream import ic
 
-PLAYERS : int = 10
-STRATEGY: int = 5
+PLAYERS     : int = 2
+STRATEGY    : int = 2
+ITERATIONS  : int = 10
+
+MIN_RANGE   : float = -1.0
+MAX_RANGE   : float = 1
 
 payoff_gen: PayoffGen = PayoffGen(PLAYERS, STRATEGY)
-payoff_gen.payoff_range(-10, 10)
+payoff_gen.payoff_range(MIN_RANGE, MAX_RANGE)
+choices: Choices = Choices(payoff_gen.choice_index_data, payoff_gen.possibilities)
+
 
 historic: list[np.ndarray] = []
 
-choices: Choices = Choices(payoff_gen.choice_index_data, payoff_gen.possibilities)
-
-for _ in range(100):
+for _ in range(ITERATIONS):
     matrix = payoff_gen.gen_random_payoff_matrix()
     
     P1 = choices.algorithms.get_maxsum(matrix[0], 0)
     P2 = choices.algorithms.get_maxsum(matrix[1], 1)
-    P3 = choices.algorithms.get_maxsum(matrix[2], 2)
-    P4 = choices.algorithms.get_maxsum(matrix[3], 3)
-    P5 = choices.algorithms.get_maxsum(matrix[4], 4)
-    P6 = choices.algorithms.get_maxsum(matrix[5], 5)
-    P7 = choices.algorithms.get_maxsum(matrix[6], 6)
-    P8 = choices.algorithms.get_maxsum(matrix[7], 7)
-    P9 = choices.algorithms.get_maxsum(matrix[8], 8)
-    P10 = choices.algorithms.get_maxsum(matrix[9], 9)
     
-    historic.append(choices.get_payoffs(matrix, (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10)))
-
+    historic.append(choices.get_payoffs(matrix, (P1, P2)))
 
 historic: np.ndarray = np.array(historic)
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-x: np.ndarray = np.arange(historic.shape[0])
-fig, ax = plt.subplots(1, 1)
-for n in range(historic.shape[1]):
-    sns.lineplot(x=x, y=np.cumsum(historic[:, n]))
-
-plt.show()
---
+++ app/nash/resources/generate_bin.py
@@ -0,0 +1,32 @@
+# |--------------------------------------------------------------------------------------------------------------------|
+# |                                                                                      app/resources/generate_bin.py |
+# |                                                                                                    encoding: UTF-8 |
+# |                                                                                                     Python v: 3.10 |
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# | Imports |----------------------------------------------------------------------------------------------------------|
+import pickle
+from pathlib import Path, PosixPath
+
+from typing import Any
+# |--------------------------------------------------------------------------------------------------------------------|
+
+PATH_: PosixPath = Path("nash")
+
+class GenBin(object):
+    @staticmethod
+    def save(name: str, obj: Any) -> None:
+        """
+        Save a object
+        """
+        with open(Path(PATH_, f"{name}.bin"), "wb") as f:
+            pickle.dump(obj, f)
+    
+    @staticmethod
+    def load(name: str) -> Any:
+        """
+        Load a object
+        """
+        with open(Path(PATH_, f"{name}.bin"), "rb") as f:
+            file: Any = pickle.load(f)
+        return file
