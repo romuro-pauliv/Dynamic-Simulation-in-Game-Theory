c6888fa0ed8821717541e0c05b5c1097e35078b6
Romulo Pauliv <romulopauliv@bk.ru>
2024-02-23 22:08:56 -0300

    added multiprocessing test

--- app/nash/__main__.py
+++ app/nash/__main__.py
@@ -2,28 +2,42 @@ from calc.payoff_generation import PayoffGen
 from controller.choices import Choices
 from resources.generate_bin import GenBin
 import numpy as np
-from icecream import ic
+from uuid import uuid4
 
-PLAYERS     : int = 2
-STRATEGY    : int = 2
-ITERATIONS  : int = 10
-
-MIN_RANGE   : float = -1.0
-MAX_RANGE   : float = 1
-
-payoff_gen: PayoffGen = PayoffGen(PLAYERS, STRATEGY)
-payoff_gen.payoff_range(MIN_RANGE, MAX_RANGE)
-choices: Choices = Choices(payoff_gen.choice_index_data, payoff_gen.possibilities)
-
-
-historic: list[np.ndarray] = []
-
-for _ in range(ITERATIONS):
-    matrix = payoff_gen.gen_random_payoff_matrix()
+def simulate() -> None:
+    PLAYERS     : int = 4
+    STRATEGY    : int = 2
+    ITERATIONS  : int = 1000000
+    MIN_RANGE   : float = -1
+    MAX_RANGE   : float = 1
+    ID_         : str   = str(uuid4())
     
-    P1 = choices.algorithms.get_maxsum(matrix[0], 0)
-    P2 = choices.algorithms.get_maxsum(matrix[1], 1)
-    
-    historic.append(choices.get_payoffs(matrix, (P1, P2)))
+    payoff_gen  : PayoffGen = PayoffGen(PLAYERS, STRATEGY)
+    choices     : Choices   = Choices(payoff_gen.choice_index_data, payoff_gen.possibilities)
+    payoff_gen.payoff_range(MIN_RANGE, MAX_RANGE)
+    historic: list[np.ndarray] = []
+    for _ in range(ITERATIONS):
+        matrix = payoff_gen.gen_random_payoff_matrix()
+        # Algorithms to each player |------------------------------------|
+        P1 = choices.algorithms.get_maxsum(matrix[0], 0)
+        P2 = choices.algorithms.get_maxsum(matrix[1], 1)
+        P3 = choices.algorithms.get_maxsum(matrix[2], 2)
+        P4 = choices.algorithms.get_maxsum(matrix[3], 3)
+        historic.append(choices.get_payoffs(matrix, (P1, P2, P3, P4)))
+        # |--------------------------------------------------------------|
+        
+    historic: np.ndarray = np.array(historic)
+    GenBin.save(ID_, historic)
 
-historic: np.ndarray = np.array(historic)
+
+import multiprocessing as mp
+
+process: list[mp.Process] = []
+for _ in range(mp.cpu_count()-1):
+    process.append(mp.Process(target=simulate))
+
+for process_ in process:
+    process_.start()
+
+for process_ in process:
+    process_.join()
